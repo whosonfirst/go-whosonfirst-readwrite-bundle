2ca3c7c396f3ddbfc46a56ef45dc0c26aacb3e41
thisisaaronland <devnull@localhost>
2018-10-18 11:26:09 -0700

    add null/stdout writers

--- bundle.go
+++ bundle.go
@@ -10,8 +10,11 @@ import (
 	mysql_reader "github.com/whosonfirst/go-whosonfirst-readwrite-mysql/reader"
 	s3_config "github.com/whosonfirst/go-whosonfirst-readwrite-s3/config"
 	s3_reader "github.com/whosonfirst/go-whosonfirst-readwrite-s3/reader"
+	s3_writer "github.com/whosonfirst/go-whosonfirst-readwrite-s3/writer"
 	sqlite_reader "github.com/whosonfirst/go-whosonfirst-readwrite-sqlite/reader"
+	sqlite_writer "github.com/whosonfirst/go-whosonfirst-readwrite-sqlite/writer"
 	"github.com/whosonfirst/go-whosonfirst-readwrite/reader"
+	"github.com/whosonfirst/go-whosonfirst-readwrite/writer"
 	_ "log"
 	"strings"
 )
@@ -56,7 +59,7 @@ func NewMultiReaderFromFlags(dsn_flags flags.MultiDSNString) (reader.Reader, err
 
 			// something something something filesystem globbing...
 			// (20180822/thisisaaronland)
-			
+
 			r, e = newFSReader(dsn)
 		case "GITHUB":
 
@@ -202,4 +205,100 @@ func dsnToString(dsn map[string]string) string {
 
 	str_dsn = strings.Trim(str_dsn, " ")
 	return str_dsn
+}
+
+func ValidWritersString() string {
+
+	valid := ValidWriters()
+	return strings.Join(valid, ", ")
+}
+
+func ValidWriters() []string {
+
+	writers := []string{
+		"fs",
+		// "github",
+		// "http",
+		// "mysql",
+		"null",
+		"s3",
+		"sqlite",
+		"stdout",
+	}
+
+	return writers
+}
+
+func NewMultiWriterFromFlags(dsn_flags flags.MultiDSNString) (writer.Writer, error) {
+
+	writers := make([]writer.Writer, 0)
+
+	for _, dsn := range dsn_flags {
+
+		source, ok := dsn["writer"]
+
+		if !ok {
+			return nil, errors.New("Missing writer key in DSN string")
+		}
+
+		var w writer.Writer
+		var e error
+
+		switch strings.ToUpper(source) {
+
+		case "FS":
+			w, e = newFSWriter(dsn)
+		case "NULL":
+			w, e = writer.NewNullWriter()
+		case "S3":
+			w, e = newS3Writer(dsn)
+		case "SQLITE":
+			w, e = newSQLiteWriter(dsn)
+		case "STDOUT":
+			w, e = writer.NewStdoutWriter()
+		default:
+			return nil, errors.New("Unsupported writer")
+		}
+
+		if e != nil {
+			return nil, e
+		}
+
+		writers = append(writers, w)
+	}
+
+	if len(writers) == 0 {
+		return nil, errors.New("You forgot to specify any sources")
+	}
+
+	return writer.NewMultiWriter(writers...)
+}
+
+func newFSWriter(dsn map[string]string) (writer.Writer, error) {
+
+	root, ok := dsn["root"]
+
+	if !ok {
+		return nil, errors.New("FS writer DSN missing a root={PATH} pair")
+	}
+
+	return writer.NewFSWriter(root)
+}
+
+func newS3Writer(dsn map[string]string) (writer.Writer, error) {
+
+	str_dsn := dsnToString(dsn)
+
+	cfg, err := s3_config.NewS3ConfigFromString(str_dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return s3_writer.NewS3Writer(cfg)
+}
+
+func newSQLiteWriter(dsn map[string]string) (writer.Writer, error) {
+	str_dsn := dsnToString(dsn)
+	return sqlite_writer.NewSQLiteWriter(str_dsn)
 }
